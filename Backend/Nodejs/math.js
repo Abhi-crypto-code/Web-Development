793630a66996b8a19ce96f040278747e7424c2ca
Abhishek Gonnade <2022chb1037@iitrpr.ac.in>
2025-03-28 19:21:27 +0530

    new files

---
+++ Backend/Nodejs/math.js
@@ -0,0 +1,9 @@
+const sum = (a,b)=> a+b;
+const div = (a,b)=>a/b;
+
+obj = {
+    sum : sum,
+    div : div,
+};
+
+module.exports = obj;
--- Backend/Nodejs/script.js
+++ Backend/Nodejs/script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+// console.log("hello");
+let value = require("./math");
+
+let sumEx = value.sum(1,2);
+console.log(sumEx);
+let div = value.div(2,1);
+console.log(div);
