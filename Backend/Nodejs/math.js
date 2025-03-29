02a8b005fa0c72c9cb358d5a58ee6239ed4bde1a
Abhishek Gonnade <2022chb1037@iitrpr.ac.in>
2025-03-29 21:52:34 +0530

    new

--- Backend/Nodejs/math.js
+++ Backend/Nodejs/math.js
@@ -1,9 +1,11 @@
-const sum = (a,b)=> a+b;
-const div = (a,b)=>a/b;
+export const sum = (a,b)=> a+b;
+export const div = (a,b)=>a/b;
+export const mul = (a,b)=>a*b;
+export const PI = 3.14;
 
-obj = {
-    sum : sum,
-    div : div,
-};
+// obj = {
+//     sum : sum,
+//     div : div,
+// };
 
-module.exports = obj;
+// module.exports = obj;
--- Backend/Nodejs/script.js
+++ Backend/Nodejs/script.js
@@ -1,9 +1,12 @@
-// // console.log("hello");
-// let value = require("./math");
+// // // console.log("hello");
+// // let value = require("./math");
 
-// let sumEx = value.sum(1,2);
-// console.log(sumEx);
-// let div = value.div(2,1);
-// console.log(div);
-const fruits = require("F");
-console.log(fruits);
+// // let sumEx = value.sum(1,2);
+// // console.log(sumEx);
+// // let div = value.div(2,1);
+// // console.log(div);
+// const fruits = require("F");
+// console.log(fruits);
+
+import {mul,PI} from "./math.js";
+console.log(PI);
