f590b72d497b5b1d876a1519dca572af0021adae
Abhishek Gonnade <2022chb1037@iitrpr.ac.in>
2025-04-23 19:31:20 +0530

    express

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
 
-let port = 8080;//3000
-app.listen(port,()=>{
+// console.dir(app);
+
+let port = 3000;
+
+app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`);
+});
+
+app.use((req,res)=>{
+    console.log("request recieved");
+
+    res.send({
+        name :"apple",
+        color : "red",
+    });
 });
