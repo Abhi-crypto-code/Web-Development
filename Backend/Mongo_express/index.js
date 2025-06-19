cdbebb68fe370185e50f7a8d70a24b854bcbde29
Abhishek Gonnade <2022chb1037@iitrpr.ac.in>
2025-06-19 23:55:20 +0530

    MongoDB with express

---
+++ Backend/Mongo_express/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+main()
+.then(()=>{
+    console.log("connection successsful");
+})
+.catch((err)=>console.log(err));
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
